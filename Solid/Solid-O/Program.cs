d7ef7cd5648d8231b3dc2dfb670a8c226d91f060
facuCoronel <facundoa.coronel@gmail.com>
2022-12-20 14:36:08 -0300

    O

---
+++ Solid/Solid-O/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Solid_O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public interface IDrink
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public decimal Invoice { get; set; }
+            public decimal GetPrice();
+        }
+
+        //utlizamos la interfaz para respetar la implementacion
+
+        public class Water : IDrink
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public decimal Invoice { get; set; }
+
+            public decimal GetPrice()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        public class Invoice
+        {
+            public decimal GetTotal(IEnumerable<IDrink> lstDrink)
+            {
+                decimal total = 0;
+                foreach(var drink in lstDrink)
+                {
+                    total += drink.Price;
+                }
+                return total;
+            }
+        }
+    }
+}
